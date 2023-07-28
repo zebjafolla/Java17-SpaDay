a3a4c1c7d35a8683cef366eb26a5b265a688b847
Sally Steuterman <sally.steuterman@gmail.com>
2023-07-28 18:01:54 -0500

    Setting up starter code for model validation

---
+++ src/main/java/org/launchcode/controllers/SpaDayController.java
@@ -0,0 +1,27 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+
+
+@Controller
+public class SpaDayController {
+
+    @GetMapping
+    public String displayClientForm (Model model) {
+        return "serviceSelection";
+    }
+
+    @PostMapping
+    public String processClientForm(@RequestParam String skintype, @RequestParam String manipedi, Model model) {
+        Client newClient = new Client(skintype, manipedi);
+        newClient.setAppropriateFacials(skintype);
+        model.addAttribute("client" , newClient);
+
+        return "menu";
+    }
+}
---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,34 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
+        }
+        else {
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
+
+    }
+
+
+}
