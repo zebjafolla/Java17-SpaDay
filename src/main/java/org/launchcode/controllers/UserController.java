3fc3d36ed95864b4c0eb79394cf99ca9f4146d81
zebjafolla <113193465+zebjafolla@users.noreply.github.com>
2024-11-11 06:24:25 -0500

    added rough validation annotations to spa user sign up form

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -10,16 +12,20 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
         model.addAttribute("user", user);
         model.addAttribute("verify", verify);
         model.addAttribute("username", user.getUsername());
         model.addAttribute("email", user.getEmail());
+//        if(errors.hasErrors()){
+//            return "user/add";
+//        }
         if (user.getPassword().equals(verify)) {
             return "user/index";
         }
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,9 +1,23 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @Size(min = 5, max = 15, message="username must be between 5 and 15 chars")
     private String username;
+
+    @Email(message = "must be proper email")
     private String email;
+
+    @NotBlank
+    @Size(min = 6, message="password must be minimum of 5 chars")
     private String password;
+
+    @NotBlank
+    private String verify;
 
     public User() {
 
@@ -38,6 +52,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
     }
 }
 
